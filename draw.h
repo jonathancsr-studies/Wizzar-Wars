ebf13cc1f844aa267ac698d79fb9f9abd7e7d4de
pandaglacial <guilhermeihara@hotmail.com>
2016-08-31 20:34:25 -0300

    Add files via upload

--- draw.h
+++ draw.h
@@ -8,9 +8,14 @@ srand(time(0));
   for(i=0;i<8;i++){
       y=200+rand()%900;
 
-      if(i==0||i==7)
+      if(i==0||i==7){
         y+=200;
-
+        if(i==0)
+          global_py=y;
+          else{
+            global_uy=y;
+          }
+      }
       if(y%2==0 && i!=0 && i!=7)
         cont-=12.5;
       glBegin(GL_TRIANGLE_STRIP);
@@ -30,4 +35,32 @@ srand(time(0));
       }
 
     glFlush();
+}
+
+void desenhaPersonagem(int personagem){
+
+      int i;
+      double altura;
+      glClear(GL_COLOR_BUFFER_BIT);
+      glColor3f(0,0,0);
+
+      if(personagem==1){
+        altura=global_uy;
+      }else{
+        altura=global_py;
+      }
+
+      for(i=0;i<4;i++){
+
+        glBegin(GL_TRIANGLE_STRIP);
+
+        glVertex2f(10,altura);
+        glVertex2f(30,altura);
+        glVertex2f(30,altura+40);
+        glVertex2f(10,altura+40);
+        glVertex2f(10,altura);
+
+        glEnd();
+      }
+      glFlush();
 }
