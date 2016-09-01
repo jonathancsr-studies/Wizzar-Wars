a20aa8b6ff073abeaa0b8ebc151ecfa13b73dc0b
pandaglacial <guilhermeihara@hotmail.com>
2016-09-01 08:38:17 -0300

    Update action.h

--- action.h
+++ action.h
@@ -1,17 +1,29 @@
-void movimentoPersonagem(int a){
+void movimentoPersonagemDireita(int a){
 
       glPushMatrix();                 // Importante!!
-
+      
       glTranslatef(5, 0);
-      if(i==1){
+      if(a==1){
       desenhaPersonagem(1);
       }else{
             desenhaPersonagem(0);
       }
       glPopMatrix();                  // Importante!!
 
-
 }
 
+void movimentoPersonagemDireita(int a){
+
+      glPushMatrix();                 // Importante!!
+      
+      glTranslatef(-5, 0);
+      if(a==1){
+      desenhaPersonagem(1);
+      }else{
+            desenhaPersonagem(0);
+      }
+      glPopMatrix();                  // Importante!!
+
+}
 //http://www.inf.pucrs.br/~pinho/CG/Aulas/OpenGL/TransformacoesGL.html
 
