64bc0fa3f98f09801e66b64092a02f5661cde61a
jonathancsr <jonathan.csr@hotmail.com>
2016-08-31 22:13:29 -0300

    Add files via upload

--- keyboard.h
+++ keyboard.h
@@ -9,11 +9,35 @@ void teclaPresionada(unsigned char key, int x, int y){
     case 82:
       //reiniciaJogo();
       break;
-
+// ESC
+    case 27:
+      exit(0);
+      break;
     case 'a':
-    //glutDisplayFunc(movimentoPersonagem);
-    movimentoPersonagem();
-    break;
+//glutDisplayFunc(movimentoPersonagem);
+      movimentoPersonagem();
+      break;
+    case 70:
+      glutFullScreen();
+    default:
+      break;
+
+  }
+
+}
+void teclaPresionada(unsigned char key, int x, int y){
+
+  switch (key) {
+// P
+    case 80:
+      //pause();
+      break;
+// R
+    case 82:
+      //reiniciaJogo();
+      break;
+
+
 // ESC
     case 27:
       exit(0);
--- menu.h
+++ menu.h
@@ -1,34 +1,51 @@
-
-
-
-
-
-void desenhaMenu()
+void mouse(int button, int state, int x, int y)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0,0,1);
-    glBegin(GL_TRIANGLE_FAN);
-      glVertex3f(0,0,0);
-      glVertex3f(1280,0,0);
-      glVertex3f(1280,768,0);
-      glVertex3f(0,768,0);
-    glEnd();
-    glColor3f(0,1,0);
-    glBegin(GL_TRIANGLE_STRIP);
-      glVertex3f(540,50,0);
-      glVertex3f(540,0,0);
-      glVertex3f(1280,768,0);
-      glVertex3f(0,768,0);
-    glEnd();
-
-    glFlush();
 
 }
 
+void criarRetangulo(double largura, double altura,double x,double y,double c0,double c1,double c2)
+{
+  glPushMatrix();
+  glTranslatef(x,y,0);
+  glColor3f(c0,c1,c2);
+  glBegin(GL_TRIANGLE_FAN);
+    glVertex3f(0,0,0);
+    glVertex3f(largura,0,0);
+    glVertex3f(largura,altura,0);
+    glVertex3f(0,altura,0);
+  glEnd();
+  glPopMatrix();
+}
+void desenhaMenu()
+{
 
+  criarRetangulo(700,700,0,0,0.1,0.2,0.3);
+  //LOGO
+  criarRetangulo(500,200,100,450,0.5,0.6,0.8);
+  //menu jogar
+  criarRetangulo(200,100,260,280,0.5,0.6,0.3);
+  //menu opções
+  criarRetangulo(200,100,260,160,0.5,0.6,0.3);
+  //menu sair
+  criarRetangulo(200,100,260,40,0.5,0.6,0.3);
+
+  glFlush();
+}
 
 void menuprincipal()
 {
 
 
+}
+
+void menu(int item)
+{
+  switch (item) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+  }
 }
