4ab323e61a816cd7f329a4f6a002c28b0abf9366
pandaglacial <guilhermeihara@hotmail.com>
2016-09-06 01:06:27 -0300

    Delete cenario.h

--- cenario.h
+++
@@ -1,9 +0,0 @@
-#ifndef CENARIO_H
-#define CENARIO_H
-
-void criarRetangulo(double largura, double altura);
-void desenhaCenaInicialJogo(PREDIOS predio[]);
-void gerarMapa(PREDIOS* predio) ;
-void planodeFundo();
-
-#endif // CENARIO_H
